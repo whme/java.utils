0b1701e22df7ca1dce34fb1362a3a2fa98fb6af5
unknown <max-hoehl@outlook.de>
2021-01-18 22:20:23 +0100

    Comment tests out

--- tests/DictTest.java
+++ tests/DictTest.java
@@ -1,26 +1,26 @@
-package tests;
+// package tests;
 
-import java.lang.reflect.Method;
+// import java.lang.reflect.Method;
 
-import utils.util.Dict;
-import utils.util.DictItem;
-import utils.util.List;
+// import utils.util.Dict;
+// import utils.util.DictItem;
+// import utils.util.List;
 
-public class DictTest extends Tester {
+// public class DictTest extends Tester {
 
-    public DictTest() {
-        super();
-        Tester.tests.add(
-            new DictItem<Object,List<Method>>(
-                this,
-                new List<Method>(this.getMethods())
-            )
-        );
-    }
+//     public DictTest() {
+//         super();
+//         Tester.tests.add(
+//             new DictItem<Object,List<Method>>(
+//                 this,
+//                 new List<Method>(this.getMethods())
+//             )
+//         );
+//     }
 
-    public void Dict_NoArgs_test() {
-        Dict<String, Integer> dict = new Dict<>();
-        List<Method> methods = new List<>(dict.getClass().getMethods());
-        System.out.print(methods);
-    }
-}
+//     public void Dict_NoArgs_test() {
+//         Dict<String, Integer> dict = new Dict<>();
+//         List<Method> methods = new List<>(dict.getClass().getMethods());
+//         System.out.print(methods);
+//     }
+// }
